5a0f20c246b309a15cbbb00e2d5134c14379d3c9
Dariusz Harańczyk <darek.haranczyk@gmail.com>
2019-01-04 10:09:27 +0100

    Dodano funkcjonalność kalkulatora.

--- src/main/java/com/kodilla/Calculator.java
+++ src/main/java/com/kodilla/Calculator.java
@@ -1,4 +1,17 @@
 package com.kodilla;
 
 public class Calculator {
+
+    double a = 0.0;
+    double b = 0.0;
+
+    public Calculator(double a, double b){
+        this.a = a;
+        this.b = b;
+    }
+
+    public double addNumbers(){
+        return  a + b;
+
+    }
 }
--- src/main/java/com/kodilla/KodillaCourseApplication.java
+++ src/main/java/com/kodilla/KodillaCourseApplication.java
@@ -1,14 +1,36 @@
 package com.kodilla;
 
+import org.omg.Messaging.SYNC_WITH_TRANSPORT;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.Scanner;
 
 @SpringBootApplication
 public class KodillaCourseApplication {
 
+
+
     public static void main(String[] args) {
         SpringApplication.run(KodillaCourseApplication.class, args);
+
+        Scanner takeA = new Scanner(System.in);
+        Scanner takeB = new Scanner(System.in);
+
+        double a = 0.0;
+        double b = 0.0;
+
+        System.out.println("Podaj liczbę A: ");
+        a = takeA.nextDouble();
+        System.out.println("Podaj liczbę B: ");
+        b = takeA.nextDouble();
+        Calculator add = new Calculator(a,b);
+
+        System.out.println("Wynik dodawania: " + a + " + " + b + " = " + add.addNumbers());
+
+
     }
+
 
 }
 
